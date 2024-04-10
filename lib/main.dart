b2cea386bf0e37b87fcbc00239e1064de0ceb369
Ismael Febles <reynaldofebles@gmail.com>
2024-04-10 17:28:28 -0400

    Initializing supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:supabase_feed/pages/create.dart';
 import 'package:supabase_feed/pages/home.dart';
 import 'package:supabase_feed/pages/update.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_feed/secrets.dart';
 
 void main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
   runApp(MaterialApp(
     routes: {
       '/': (ctx) => const Home(),
