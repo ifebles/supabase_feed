2f34f7f4b548c35a4a38ac380485dd59490c4c05
Ismael Febles <reynaldofebles@gmail.com>
2024-04-11 10:01:50 -0400

    Adding data fetch and presentation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_feed/widgets/list_entry.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,10 +10,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final client = Supabase.instance.client;
+  var isLoading = true;
+  List<Map<String, dynamic>> data = [];
+
+  void fetchData() async {
+    var result = await client.from('activity').select().catchError((error) {
+      print(error);
+      return [];
+    });
+
+    setState(() {
+      isLoading = false;
+      data = result;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget body;
+
+    if (isLoading) {
+      body = const Center(child: CircularProgressIndicator());
+    } else if (data.isEmpty) {
+      body = Center(
+        child: Text(
+          'The list is empty',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.grey[400],
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      );
+    } else {
+      body = ListView.separated(
+        separatorBuilder: (context, index) => const Divider(height: 20),
+        itemCount: data.length,
+        itemBuilder: (context, index) => ListEntry(data[index]),
+      );
+    }
+
     return Scaffold(
-      body: const SafeArea(child: Text('Home')),
+      body: SafeArea(child: body),
       floatingActionButton: IconButton(
         onPressed: () {
           Navigator.pushNamed(context, '/create');
